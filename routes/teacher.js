a2533454a44c7e8cdc599e63bcfaabc85b92233a
Edrian Jose Ferrer <edrianjoseferrer@gmail.com>
2020-03-13 18:34:28 +0800

    created PUT /api/teachers/:id, /api/teachers/:id/res..

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
+
 const { Teacher, validateTeacher } = require("../models/teacher");
 const SystemLog = require("../models/log");
 router.get("/", async (req, res) => {
@@ -38,7 +40,8 @@ router.post("/", async (req, res) => {
   });
   if (duplicateTeacher)
     return res.status(400).send("Bad request, name already registered");
-  //TODO: encrypt password
+  const salt = await bcrypt.genSalt(10);
+  req.body.password = await bcrypt.hash(req.body.password, salt);
   const teacher = new Teacher(req.body);
   await teacher.save();
   const msg = `${
@@ -48,13 +51,59 @@ router.post("/", async (req, res) => {
   res.send(teacher);
 });
 router.get("/:id", async (req, res) => {
-  res.status(200).send("req");
+  const teacher_information = await Teacher.findById(req.params.id);
+  if (!teacher_information)
+    return res.status(400).send("Bad request, employee doesn't exist.");
+  const msg = `${
+    req.user.name
+  } query ${teacher_information.fullname()} data in database`;
+  await new SystemLog(SystemLog.createLog(req, res, msg)).save();
+  res.send(teacher_information);
 });
 router.put("/:id", async (req, res) => {
-  res.status(200).send("req");
+  req.body.password = "default";
+  const { error } = validateTeacher(req.body);
+  if (error) return res.status(400).send("Bad request, invalid teacher object");
+
+  let teacher = await Teacher.findById(req.params.id);
+
+  const duplicateEmployeeNum = await Teacher.findOne({
+    _id: { $not: { $eq: req.params.id } },
+    employee_number: req.body.employee_number
+  });
+
+  if (duplicateEmployeeNum) {
+    return res
+      .status(400)
+      .send("Bad request, employee_number already registered");
+  }
+
+  const duplicateTeacher = await Teacher.findOne({
+    _id: { $not: { $eq: req.params.id } },
+    "name.last": req.body.name.last,
+    "name.first": req.body.name.first,
+    "name.middle": req.body.name.middle
+  });
+  if (duplicateTeacher)
+    return res.status(400).send("Bad request, name already registered");
+  req.body.password = teacher.password;
+  teacher = await Teacher.findByIdAndUpdate(req.params.id, req.body);
+  const msg = `${req.user.name} updates ${teacher.fullname()} data in database`;
+  await new SystemLog(SystemLog.createLog(req, res, msg)).save();
+  res.send(teacher);
 });
+
 router.put("/:id/resetpassword", async (req, res) => {
-  res.status(200).send("req");
+  const teacher = await Teacher.findById(req.params.id);
+  if (!teacher)
+    return res.status(400).send("Bad request, teacher doesn't exist");
+
+  const salt = await bcrypt.genSalt(10);
+  teacher.password = await bcrypt.hash(teacher.employee_number, salt);
+  await teacher.save();
+  const msg = `${req.user.name} resets ${teacher.fullname()} password`;
+  await new SystemLog(SystemLog.createLog(req, res, msg)).save();
+  res.send(teacher.fullname());
 });
 router.post("/:id/logs", async (req, res) => {
   res.status(200).send("req");
--- tests/integration/routes/teacher.test.js
+++ tests/integration/routes/teacher.test.js
@@ -95,12 +95,6 @@ describe("POST /api/teachers/", () => {
       .send(teacher);
     expect(parseInt(res.body.employee_number)).toBe(teacher.employee_number);
   });
-  //if unauthenticated /
-  //if unauthorized /
-  //if teacher obj is invalid /
-  //if employee number already registered /
-  //if teacher already registered /
-  //if successful execution
 });
 
 describe("GET /api/teachers", () => {
@@ -126,8 +120,6 @@ describe("GET /api/teachers", () => {
         }
       ]
     };
-    // teacherDocument = new Teacher(teacher);
-    // await teacherDocument.save();
     teacherIds = [];
     for (let i = 0; i < 10; i++) {
       teacherIds.push(mongoose.Types.ObjectId());
@@ -163,5 +155,217 @@ describe("GET /api/teachers", () => {
       .get("/api/teachers")
       .set("x-auth-token", token);
     expect(res.body.length).toBe(teacherIds.length);
+  });
+});
+
+describe("GET /api/teachers/:id", () => {
+  let token, teacher, admin, teacherDocument, teacherId;
+  beforeEach(async () => {
+    admin = {
+      _id: mongoose.Types.ObjectId().toHexString(),
+      assignments: [{ category: "Admin" }]
+    };
+    teacher = {
+      name: {
+        first: "Edrian",
+        middle: "De Guzman",
+        last: "Ferrer"
+      },
+      birthdate: "2000-01-12T16:00:00.000Z",
+      gender: "Male",
+      employee_number: 1,
+      password: "1234567",
+      assignments: [
+        {
+          category: "Subject Teacher"
+        }
+      ]
+    };
+    teacherDocument = new Teacher(teacher);
+    await teacherDocument.save();
+    teacherId = teacherDocument._id;
+  });
+
+  afterEach(async () => {
+    await Teacher.deleteMany({});
+  });
+
+  it("should return 401 if unauthenticated", async () => {
+    const res = await request(server).get(
+      `/api/teachers/${teacherId.toHexString()}`
+    );
+    expect(res.status).toBe(401);
+  });
+
+  it("should return 403 if unauthorized", async () => {
+    admin.assignments[0].category = "Subject Teacher";
+    token = new Teacher(admin).generateAuthToken();
+    const res = await request(server)
+      .get(`/api/teachers/${teacherId.toHexString()}`)
+      .set("x-auth-token", token);
+    expect(res.status).toBe(403);
+  });
+
+  it("should return teacher info if successful", async () => {
+    token = new Teacher(admin).generateAuthToken();
+    const res = await request(server)
+      .get(`/api/teachers/${teacherId.toHexString()}`)
+      .set("x-auth-token", token);
+    expect(res.body._id).toBe(teacherId.toHexString());
+  });
+});
+
+describe("PUT /api/teachers/:id", () => {
+  let token, teacher, admin, teacherDocument;
+  beforeEach(async () => {
+    admin = {
+      _id: mongoose.Types.ObjectId().toHexString(),
+      assignments: [{ category: "Admin" }]
+    };
+    teacher = {
+      name: {
+        first: "Edrian Jose",
+        middle: "De Guzman",
+        last: "Ferrer"
+      },
+      birthdate: "2000-01-12T16:00:00.000Z",
+      gender: "Male",
+      employee_number: 9999998,
+      password: "9999998"
+    };
+    teacherDocument = new Teacher(teacher);
+    await teacherDocument.save();
+    teacher.name.first = "Erika May";
+    teacher.employee_number = 9999999;
+    teacherDocument = new Teacher(teacher);
+    await teacherDocument.save();
+  });
+
+  afterEach(async () => {
+    await Teacher.deleteMany({});
+  });
+
+  it("should return 401 if unauthenticated", async () => {
+    const res = await request(server).put(
+      `/api/teachers/${teacherDocument._id.toHexString()}`
+    );
+    expect(res.status).toBe(401);
+  });
+
+  it("should return 403 if unauthorized", async () => {
+    admin.assignments[0].category = "Subject Teacher";
+    token = new Teacher(admin).generateAuthToken();
+    const res = await request(server)
+      .put(`/api/teachers/${teacherDocument._id.toHexString()}`)
+      .set("x-auth-token", token);
+
+    expect(res.status).toBe(403);
+  });
+
+  it("should return 400 if teacher obj is invalid", async () => {
+    token = new Teacher(admin).generateAuthToken();
+    delete teacher.birthdate;
+    const res = await request(server)
+      .put(`/api/teachers/${teacherDocument._id.toHexString()}`)
+      .set("x-auth-token", token)
+      .send(teacher);
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 400 if employee number is already registered", async () => {
+    teacher.employee_number = 9999998;
+    token = new Teacher(admin).generateAuthToken();
+    const res = await request(server)
+      .put(`/api/teachers/${teacherDocument._id.toHexString()}`)
+      .set("x-auth-token", token)
+      .send(teacher);
+
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 400 if teacher is already registered", async () => {
+    teacher.name.first = "Edrian Jose";
+    token = new Teacher(admin).generateAuthToken();
+    const res = await request(server)
+      .put(`/api/teachers/${teacherDocument._id.toHexString()}`)
+      .set("x-auth-token", token)
+      .send(teacher);
+    expect(res.status).toBe(400);
+  });
+
+  it("should return created teacher if successful", async () => {
+    token = new Teacher(admin).generateAuthToken();
+    const res = await request(server)
+      .put(`/api/teachers/${teacherDocument._id.toHexString()}`)
+      .set("x-auth-token", token)
+      .send(teacher);
+    expect(parseInt(res.body.employee_number)).toBe(teacher.employee_number);
+  });
+});
+
+describe("PUT /api/teachers/:id/resetpassword", () => {
+  let token, teacher, admin, teacherDocument, teacherId;
+  beforeEach(async () => {
+    admin = {
+      _id: mongoose.Types.ObjectId().toHexString(),
+      assignments: [{ category: "Admin" }]
+    };
+    teacher = {
+      name: {
+        first: "Edrian",
+        middle: "De Guzman",
+        last: "Ferrer"
+      },
+      birthdate: "2000-01-12T16:00:00.000Z",
+      gender: "Male",
+      employee_number: 1,
+      password: "1234567",
+      assignments: [
+        {
+          category: "Subject Teacher"
+        }
+      ]
+    };
+    teacherDocument = new Teacher(teacher);
+    await teacherDocument.save();
+    teacherId = teacherDocument._id;
+  });
+
+  afterEach(async () => {
+    await Teacher.deleteMany({});
+  });
+
+  it("should return 401 if unauthenticated", async () => {
+    const res = await request(server).put(
+      `/api/teachers/${teacherId.toHexString()}/resetpassword`
+    );
+    expect(res.status).toBe(401);
+  });
+
+  it("should return 403 if unauthorized", async () => {
+    admin.assignments[0].category = "Subject Teacher";
+    token = new Teacher(admin).generateAuthToken();
+    const res = await request(server)
+      .put(`/api/teachers/${teacherId.toHexString()}/resetpassword`)
+      .set("x-auth-token", token);
+    expect(res.status).toBe(403);
+  });
+
+  it("should return 400 if teacher doesn't exist", async () => {
+    token = new Teacher(admin).generateAuthToken();
+    const res = await request(server)
+      .put(
+        `/api/teachers/${mongoose.Types.ObjectId().toHexString()}/resetpassword`
+      )
+      .set("x-auth-token", token);
+    expect(res.status).toBe(400);
+  });
+
+  it("should return teacher fullname if successful", async () => {
+    token = new Teacher(admin).generateAuthToken();
+    const res = await request(server)
+      .put(`/api/teachers/${teacherId.toHexString()}/resetpassword`)
+      .set("x-auth-token", token);
+    expect(res.text).toBe(teacherDocument.fullname());
   });
 });
