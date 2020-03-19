f054748a766a4135cefefe496d33fe194bb8685b
Edrian Jose Ferrer <edrianjoseferrer@gmail.com>
2020-03-19 13:17:43 +0800

    inser logging to mongodb

--- startup/logging.js
+++ startup/logging.js
@@ -1,6 +1,6 @@
 const winston = require("winston");
 const config = require("config");
-// require("winston-mongodb").MongoDB;
+require("winston-mongodb").MongoDB;
 const { createLogger, format, transports } = winston;
 const { combine, timestamp, label, printf, colorize, simple } = format;
 // require('winston-mongodb');
@@ -33,13 +33,13 @@ module.exports = function() {
         filename: "./logs/errors.log",
         handleExceptions: true
       })
-      // new winston.transports.MongoDB({
-      //   db: config.get("db"),
-      //   level: "info",
-      //   options: {
-      //     useUnifiedTopology: true
-      //   }
-      // })
+      new winston.transports.MongoDB({
+        db: config.get("db"),
+        level: "info",
+        options: {
+          useUnifiedTopology: true
+        }
+      })
     ]
   });
 
